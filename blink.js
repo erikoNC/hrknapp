5bdf357771e40bbfa80f505b07326fccd59e0cc8
Erik Prøis Olsnes <eriko@nomaceriko.local>
2017-12-04 19:57:45 +0100

    webcam, fetch

--- blink.js
+++ blink.js
@@ -1,7 +1,8 @@
 var five = require('johnny-five');
 var board = new five.Board();
 var Player = require('player');
-const DESPACITO = './despacito_cut.mp3';
+const DESPACITO = '../despacito_cut.mp3';
+var fetch = require('node-fetch');
 
 
 var NodeWebcam = require('node-webcam');
@@ -9,9 +10,9 @@ var NodeWebcam = require('node-webcam');
 var webcamOpts = {
 
     //Picture related
-    width: 1280,
-    height: 720,
-    quality: 100,
+    width: 20,
+    height: 20,
+    quality: 5,
 
     //Delay to take shot
     delay: 0,
@@ -31,7 +32,7 @@ var webcamOpts = {
 
     // [location, buffer, base64]
     // Webcam.CallbackReturnTypes
-    callbackReturn: "base64",
+    callbackReturn: 'base64',
 
     //Logging
     verbose: false
@@ -44,19 +45,39 @@ board.on('ready', function() {
   var despacitoButton = new five.Button(2);
   var led = new five.Led(13);
   var player = new Player(DESPACITO);
-  // player.add('./despacito.mp3');
 
   player.on('error', function(err){
-    // when error occurs
     console.log(err);
   });
 
   var isPlaying = false;
   despacitoButton.on('press', function() {
-      !isPlaying ? player.play() : player.stop()
-      isPlaying = !isPlaying;
-      webcam.capture('test', function(err, data) {
-          console.log(data)
+    !isPlaying ? player.play() : player.stop()
+    isPlaying = !isPlaying;
+    webcam.capture('test', function(err, data) {
+        makeRequest(data);
       });
+    })
   })
-})
+
+function makeRequest(img) {
+  const url = 'http://10.22.200.61:8080/push';
+
+  var data = {
+    title: "NYANSATT!!!",
+    content: "NYANSATT!!!",
+    image: img
+  }
+
+  console.log(data);
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      Authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJ1bmlxdWVfbmFtZSI6IkpvYXJrIn0.0_NFtDwF3WdDkizkRMiZNl947GAesQ1vXsL9qbYrLdg',
+      'Content-Type': 'application/json'
+    }
+  }).then(function(res){
+    console.log(res);
+  })
+}
